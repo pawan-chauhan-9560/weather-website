48ce782acaddcc282fffe3cf14e22e4db4db0913
pawan <pawanchauhan933@gmail.com>
2020-12-25 00:34:23 -0800

    first Commit

---
+++ public/js/main.js
@@ -0,0 +1,58 @@
+// this is searchbar city name
+const cityName = document.getElementById("cityName");
+
+//this is OutPut City name
+const city_name = document.getElementById("city_name");
+const submitBtn = document.getElementById("submitBtn");
+
+const temp_real_val = document.getElementById("temp_real_val");
+const temp_status = document.getElementById("temp_status");
+const data_hide = document.querySelector(".middle_layer");
+
+const getInfo = async (e) => {
+  e.preventDefault();
+  let cityVal = cityName.value;
+
+  if (cityVal === "") {
+    city_name.innerText = "Please write the Correct city name before search";
+    data_hide.classList.add("data_hide");
+  } else {
+    try {
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=690d4c4d35650ae693f92b68bc131ca4`;
+      const response = await fetch(url);
+
+      //get data in json
+      const data = await response.json();
+
+      //convert data into array
+      const arrData = [data];
+
+      city_name.innerText = `${arrData[0].name},${arrData[0].sys.country}`;
+
+      temp_real_val.innerText = `${arrData[0].main.temp}`;
+
+      const tempMood = arrData[0].weather[0].main;
+      // Condition to check sunny or cloudy
+      if (tempMood == "Clear") {
+        temp_status.innerHTML =
+          "<i class='fas fa-sun' style='color:#eccc68'></i>";
+      } else if (tempMood == "Clouds") {
+        temp_status.innerHTML =
+          "<i class='fas fa-cloud' style='color:#f1f2f6'></i>";
+      } else if (tempMood == "Rain") {
+        temp_status.innerHTML =
+          "<i class='fas fa-cloud-rain' style='color:#a4b0be'></i>";
+      } else {
+        temp_status.innerHTML =
+          "<i class='fas fa-sun' style='color:#eccc68'></i>";
+      }
+      data_hide.classList.remove("data_hide");
+    } catch {
+      city_name.innerText = "Please enter city name properly";
+      data_hide.classList.add("data_hide");
+    }
+  }
+};
+
+submitBtn.addEventListener("click", getInfo);
+// 2.2
---
+++ src/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+var cors = require("cors");
+const hbs = require("hbs");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 8000;
+
+//public static path
+
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", template_path);
+hbs.registerPartials(partials_path);
+
+app.use(express.static(static_path));
+//link connect to index.html
+app.use(cors());
+//Routing
+app.get("", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/weather", (req, res) => {
+  res.render("weather");
+});
+app.get("/notes", (req, res) => {
+  res.render("notes");
+});
+
+app.get("*", (req, res) => {
+  res.render("error");
+});
+
+//listening port
+app.listen(port, () => {
+  console.log(`You are listening to the port no ok ${port}`);
+});
